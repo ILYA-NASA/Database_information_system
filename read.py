e6ea1b6519fb81e14b341ae0e94171e619d3159e
SnezhannaPristavka <gorlovasv41@mail.ru>
2022-06-16 13:02:05 +0300

    add def

--- read.py
+++ read.py
@@ -1,14 +1,23 @@
 # СМОТРЕЛА КАК РАБОТАТЬ С CSV
-f = open('database.csv','w',encoding='utf-8')
-f.writelines('id')
-f.writelines(';')
-f.writelines('name' + '\n')
-f.writelines('123')
-f.writelines('name' + '\n')
-f.close()
+# f = open('database.csv','w',encoding='utf-8')
+# f.writelines('id')
+# f.writelines(';')
+# f.writelines('name' + '\n')
+# f.writelines('123')
+# f.writelines(';')
+# f.writelines('name' + '\n')
+# f.close()
 
-f = open('database.csv', 'r', encoding='utf-8')
-data = list(f.read())
-f.close()
-
-print(data)
+def read_file(file, mod):
+    f = open(file, mod) #encoding='utf-8')
+    data = f.read().splitlines()
+    list = []
+    for line in data:
+        line = line.split(';')
+        list.append(line)
+    f.close()
+    print(list[1,1])
+    return list
+# if __name__== '__main__':
+data = 'processing_model.csv'
+print(read_file(data, 'r'))
