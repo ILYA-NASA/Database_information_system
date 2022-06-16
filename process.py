a2efeced39064dc3ebd73c1e3db7a9f90cc0b6ff
Илья Насалевич <il.nasa@outlook.com>
2022-06-16 13:49:40 +0300

    correct from seminar 8

--- process.py
+++ process.py
@@ -1,15 +1,14 @@
 from User_Interface import user_choice
 
 
-def data_reader():
-    data = open('processing_model.txt', 'r', encoding= 'utf-8')
-    data = data.read().splitlines()
-    a = []
-    for i in data:
-        i = i.split(',')
-        a.append(i)
-    return a
-
+# def data_reader():
+#     data = open('processing_model.txt', 'r', encoding= 'utf-8')
+#     data = data.read().splitlines()
+#     a = []
+#     for i in data:
+#         i = i.split(',')
+#         a.append(i)
+#     return a
 
 
 # print(data_reader())
@@ -20,6 +19,8 @@ def data_reader():
 #     line = ' '.join(line)
 #     print(line)
 
+from work_file import read_file
+
 def data_processor(lst, n):
     a = []
     if n == 6:
@@ -29,6 +30,7 @@ def data_processor(lst, n):
             a.append(i[n-1])
         return a
     else:
-        return 'До встречи!' 
+        return 'До встречи!'
 
-print(data_processor(data_reader(), user_choice()))
+data = 'processing_model.csv'
+print(data_processor(read_file(data, 'r'), user_choice()))
--- work_file.py
+++ work_file.py
@@ -9,7 +9,7 @@
 # f.close()
 
 def read_file(file, mod):
-    f = open(file, mod) #encoding='utf-8')
+    f = open(file, mod) # encoding='utf-8')
     data = f.read().splitlines()
     list = []
     for line in data:
@@ -18,5 +18,5 @@ def read_file(file, mod):
     f.close()
     return list
 # if __name__== '__main__':
-data = 'processing_model.csv'
-print(read_file(data, 'r'))
+# data = 'processing_model.csv'
+# print(read_file(data, 'r'))
