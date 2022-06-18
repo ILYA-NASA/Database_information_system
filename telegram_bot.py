725e70558b7cdf3dc40730e5cbed6c6f865b2de9
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-06-18 16:22:57 +0500

    Configured the interface

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,23 +1,18 @@
-# Настроить кирилицу.
 # Указать командам исполняющие методы.
-# Интерфейс.
 
-
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram_bot_commands import *
 
 app = ApplicationBuilder().token("5157594801:AAFHHasq8CBNBoT5y6V55vvkMYuawHJk4_8").build()
 
-app.add_handler(CommandHandler("hello", hello_command))             # Приветствие
-app.add_handler(CommandHandler("help", help_command))               # Показать список команд
-app.add_handler(CommandHandler("ID", id_command))                   # 1 - вывод ID
-app.add_handler(CommandHandler("FIO", fio_command))                 # 2 - вывод ФИО
-# app.add_handler(CommandHandler("Birthday", birthday_command))     # 3 - вывод ДР
-# app.add_handler(CommandHandler("Progress", progress_command))     # 4 - вывод успеваемости
-# app.add_handler(CommandHandler("Sex", sex_command))               # 5 - вывод пола
-# app.add_handler(CommandHandler("All data", all_data_command))     # 6 - вывод всех данных
-# app.add_handler(CommandHandler("Exit", exit_command))             # 0 - выход
+app.add_handler(CommandHandler("f1", help_command))          # Показать список команд
+app.add_handler(CommandHandler("1", id_command))             # 1 - вывод ID
+app.add_handler(CommandHandler("2", fio_command))            # 2 - вывод ФИО
+app.add_handler(CommandHandler("3", birthday_command))       # 3 - вывод ДР
+app.add_handler(CommandHandler("4", progress_command))       # 4 - вывод успеваемости
+app.add_handler(CommandHandler("5", sex_command))            # 5 - вывод пола
+app.add_handler(CommandHandler("6", all_data_command))       # 6 - вывод всех данных
+app.add_handler(CommandHandler("0", exit_command))           # 0 - выход
 
 print('server start')
 
