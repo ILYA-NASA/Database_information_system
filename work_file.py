975dc671d2f01464bb33fa7a8b304d67746cd13d
SnezhannaPristavka <gorlovasv41@mail.ru>
2022-06-18 13:25:42 +0300

    add def write_file

--- work_file.py
+++ work_file.py
@@ -1,15 +1,6 @@
-# СМОТРЕЛА КАК РАБОТАТЬ С CSV
-# f = open('database.csv','w',encoding='utf-8')
-# f.writelines('id')
-# f.writelines(';')
-# f.writelines('name' + '\n')
-# f.writelines('123')
-# f.writelines(';')
-# f.writelines('name' + '\n')
-# f.close()
-
-def read_file(file, mod):
-    f = open(file, mod) # encoding='utf-8')
+list = [['1', 'Креп Анатолий Евген', '19.11.1999', 'отл', 'муж'], ['2', 'Анат Ната Конст', '22.03.2000', 'средн', 'жен'], ['3', 'Контр Кат Стан', '02.01.2001', 'отл', 'муж']]
+def read_file(file):
+    f = open(file, 'r') # encoding='utf-8')
     data = f.read().splitlines()
     list = []
     for line in data:
@@ -17,6 +8,24 @@ def read_file(file, mod):
         list.append(line)
     f.close()
     return list
-# if __name__== '__main__':
 # data = 'processing_model.csv'
-# print(read_file(data, 'r'))
+# print(read_file(data))
+
+def write_file(list, mod):
+    f = open('result.csv', mod)
+    # for line in list:
+    #     f.writelines(line)  ДЛЯ ОБЫЧНОГО СПИСКА
+    #     f.writelines(';')
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            f.writelines(list[i][j])
+            f.writelines(';')
+        f.writelines('\n')
+    f.close()
+    if mod == 'a':
+        print('Данные записаны!')
+    if mod == 'w':
+        print('Данные перезаписаны!')
+
+mod = 'a'
+write_file(list, mod)
