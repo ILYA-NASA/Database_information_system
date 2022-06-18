dea224ca9709d98c8719b9d854eb9a7e0af8c882
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-06-18 16:23:32 +0500

    Configured the interface

--- telegram_bot_commands.py
+++ telegram_bot_commands.py
@@ -1,11 +1,9 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ContextTypes
 
-async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'/Hello\n/Help\n/ID\n/FIO\n/Birthday\n/Progress\n/Sex\n/All_data\n/Exit')
+    await update.message.reply_text(f'/1 - ID\n/2 - ФИО\n/3 - День рождения\n/4 - Успеваемость\n/5 - Пол\n/6 - Все данные\n/0 - Выход')
 
 async def id_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'12345')
@@ -13,17 +11,17 @@ async def id_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def fio_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Иван Иваныч')
 
-# async def birthday_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def birthday_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'29.02.1970')
 
-# async def progress_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def progress_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Успевает')
 
-# async def sex_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def sex_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Мужской')
 
-# async def all_data_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def all_data_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Какие-то все данные')
 
-# async def exit_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def exit_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Всем пока')
