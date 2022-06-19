13b45d524000d91eb4e4286826655c920d6f6e4a
Илья Насалевич <il.nasa@outlook.com>
2022-06-19 13:54:54 +0300

    MVP is done

--- controller.py
+++ controller.py
@@ -1,6 +1,9 @@
 from User_Interface import user_choice
 from  process import data_processor
-from work_file import read_file
+from work_file import *
 
 data = 'processing_model.csv'
-print(data_processor(read_file(data, 'r'), user_choice()))
+user_list = data_processor(read_file(data), user_choice())
+print(user_list)
+
+write_file(user_list, mod='a')
